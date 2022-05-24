4104dcdcdf77e1585b69f05095a26effe06b6247
Igor Tudisco <igortudisco@hotmail.com>
2022-05-24 20:19:07 -0300

    Start products-list component

---
+++ src/app/common/product.ts
@@ -0,0 +1,13 @@
+export class Product {
+
+  sku?: string;
+  name?: string;
+  description?: string;
+  unitPrice?: number;
+  imageUrl?: string;
+  active?: boolean;
+  unitInStock?: number;
+  dateCreated?: Date;
+  lastUpdated?: Date;
+
+}
---
+++ src/app/components/products-list/products-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-products-list',
+  templateUrl: './products-list-table.component.html',
+  // templateUrl: './products-list.component.html',
+  styleUrls: ['./products-list.component.css']
+})
+export class ProductsListComponent implements OnInit {
+
+  products?: Product[];
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+  };
+
+}
